660b7a84b5cf5d6050e4eca6f47c44f2dd32ec2e
Cass <kasull88@gmail.com>
2011-11-28 11:10:09 -0500

    GObjectize viewport class

--- sinister/viewport.py
+++ sinister/viewport.py
@@ -1,5 +1,18 @@
-class Viewport(object):
+from gi.repository import GObject
+
+class Viewport(GObject.Object):
+    min_x = GObject.property(type=float)
+    max_x = GObject.property(type=float)
+    min_y = GObject.property(type=float)
+    max_y = GObject.property(type=float)
+    x_ticks = GObject.property(type=float, minimum=0)
+    y_ticks = GObject.property(type=float, minimum=0)
+    
     def __init__(self, min_x, max_x, min_y, max_y, x_ticks, y_ticks):
+        super().__init__()
+        
         self.min_x, self.max_x = min_x, max_x
         self.min_y, self.max_y = min_y, max_y
         self.x_ticks, self.y_ticks = x_ticks, y_ticks
+
+GObject.type_register(Viewport)
